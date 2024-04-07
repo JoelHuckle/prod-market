23d348e0f0935ae0c2f42888dccd55785e2b999e
Joel Huckle <joelhuckle2004@gmail.com>
2024-04-07 09:37:07 +0100

    removed backend

--- backend/controllers/user.js
+++
@@ -1,7 +0,0 @@
-exports.getUser = (req, res) => {
-  res.json(req.user);
-};
-
-exports.getUserState = (req, res) => {
-  res.json({ status: !!req.user });
-};
--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/user");
-
-router.get("/getUser", userController.getUser);
-router.get("/getUserState", userController.getUserState);
-
-module.exports = router;
