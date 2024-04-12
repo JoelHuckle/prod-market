031c693f5648bec3c8aa461105192028b6cd3428
Joel Huckle <joelhuckle2004@gmail.com>
2024-04-12 10:40:52 +0100

    clerk webhook edits

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -6,7 +6,8 @@ import { Webhook } from "svix";
 
 import { createUser } from "@/lib/actions/user.action";
 
-export async function POST(req) {
+export async function POST() {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -16,14 +17,14 @@ export async function POST(req) {
   }
 
   // Get the headers
-  const headerPayload = headers(req);
+  const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response("Error occurred -- no svix headers", {
       status: 400,
     });
   }
@@ -46,7 +47,7 @@ export async function POST(req) {
     });
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    return new Response("Error occurred", {
       status: 400,
     });
   }
@@ -55,15 +56,15 @@ export async function POST(req) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  // create user in mongodb
-  if (eventType === "user.created") {
+  // Create user in mongodb
+  if (evt.type === "user.created") {
     const { id, email_addresses, image_url, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username,
       icon: image_url,
+      username: username,
     };
 
     console.log(user);
@@ -81,7 +82,7 @@ export async function POST(req) {
     return NextResponse.json({ message: "New user created", user: newUser });
   }
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
 
   return new Response("", { status: 200 });
